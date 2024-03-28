13090a5638037d13acc8662420c47584473c7398
Piyush Jain <piyushjain@duck.com>
2024-03-28 14:29:26 -0700

    Added Sagemaker Endpoint import tests.

--- libs/aws/tests/unit_tests/test_embeddings.py
+++
@@ -1,9 +0,0 @@
-"""Test embedding model integration."""
-
-
-from langchain_aws.embeddings import BedrockEmbeddings
-
-
-def test_initialization() -> None:
-    """Test embedding model initialization."""
-    BedrockEmbeddings()
--- libs/aws/tests/unit_tests/test_vectorstores.py
+++
@@ -1,6 +0,0 @@
-from langchain_aws.vectorstores import BedrockVectorStore
-
-
-def test_initialization() -> None:
-    """Test integration vectorstore initialization."""
-    BedrockVectorStore()
