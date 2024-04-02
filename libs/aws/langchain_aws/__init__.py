f8a3ec35c4da6cd633711e41a20cb110fcb9123e
Piyush Jain <piyushjain@duck.com>
2024-04-02 12:30:50 -0700

    Added retrievers, moved gitignore to root.

--- libs/aws/langchain_aws/__init__.py
+++ libs/aws/langchain_aws/__init__.py
@@ -1,3 +1,7 @@
 from langchain_aws.llms import SagemakerEndpoint
+from langchain_aws.retrievers import AmazonKendraRetriever
 
-__all__ = ["SagemakerEndpoint"]
+__all__ = [
+    "SagemakerEndpoint",
+    "AmazonKendraRetriever"
+]
