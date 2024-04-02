867277e9c7ce69230bd157989203f3ea83753396
Piyush Jain <piyushjain@duck.com>
2024-04-02 12:30:50 -0700

    Added retrievers, moved gitignore to root.

---
+++ libs/aws/langchain_aws/retrievers/__init__.py
@@ -0,0 +1,8 @@
+from langchain_aws.retrievers.kendra import AmazonKendraRetriever
+from langchain_aws.retrievers.bedrock import AmazonKnowledgeBasesRetriever
+
+__all__ = [
+    "AmazonKendraRetriever"
+    "AmazonKnowledgeBasesRetriever"
+]
+
