eece50afd1c5f6ace69d3ccc3ebeec63a7b57bec
Piyush Jain <piyushjain@duck.com>
2024-04-03 20:01:18 -0700

    Added Bedrock LLM.

--- libs/aws/langchain_aws/llms/__init__.py
+++ libs/aws/langchain_aws/llms/__init__.py
@@ -1,3 +1,4 @@
+from langchain_aws.llms.bedrock import ALTERNATION_ERROR, Bedrock, BedrockLLM
 from langchain_aws.llms.sagemaker_endpoint import SagemakerEndpoint
 
-__all__ = ["SagemakerEndpoint"]
+__all__ = ["ALTERNATION_ERROR", "Bedrock", "BedrockLLM", "SagemakerEndpoint"]
