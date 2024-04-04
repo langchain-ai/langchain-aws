827680c3ee1fe30def6f66f2e416d9960791b41d
Piyush Jain <piyushjain@duck.com>
2024-04-03 21:32:48 -0700

    Added bedrock chat, fixed usage and tests.

--- libs/aws/langchain_aws/llms/__init__.py
+++ libs/aws/langchain_aws/llms/__init__.py
@@ -1,4 +1,15 @@
-from langchain_aws.llms.bedrock import ALTERNATION_ERROR, Bedrock, BedrockLLM
+from langchain_aws.llms.bedrock import (
+    ALTERNATION_ERROR,
+    Bedrock,
+    BedrockBase,
+    BedrockLLM,
+)
 from langchain_aws.llms.sagemaker_endpoint import SagemakerEndpoint
 
-__all__ = ["ALTERNATION_ERROR", "Bedrock", "BedrockLLM", "SagemakerEndpoint"]
+__all__ = [
+    "ALTERNATION_ERROR",
+    "Bedrock",
+    "BedrockBase",
+    "BedrockLLM",
+    "SagemakerEndpoint",
+]
