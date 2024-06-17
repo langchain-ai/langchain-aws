923a133a422bc7290ae9663b67339055740f8859
Bagatur <baskaryan@gmail.com>
2024-06-16 18:52:11 -0700

    poetry

---
+++ libs/aws/tests/unit_tests/chat_models/test_bedrock_converse.py
@@ -0,0 +1,400 @@
+"""Test chat model integration."""
+
+from typing import Any, Callable, Dict, Literal, Type, cast
+
+import pytest
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, ToolMessage
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.runnables import RunnableBinding
+from langchain_core.tools import BaseTool
+
+from langchain_aws import ChatBedrock
+from langchain_aws.chat_models.bedrock import (
+    _format_anthropic_messages,
+    _merge_messages,
+)
+from langchain_aws.function_calling import convert_to_anthropic_tool
+
+
+def test__merge_messages() -> None:
+    messages = [
+        SystemMessage("foo"),  # type: ignore[misc]
+        HumanMessage("bar"),  # type: ignore[misc]
+        AIMessage(  # type: ignore[misc]
+            [
+                {"text": "baz", "type": "text"},
+                {
+                    "tool_input": {"a": "b"},
+                    "type": "tool_use",
+                    "id": "1",
+                    "text": None,
+                    "name": "buz",
+                },
+                {"text": "baz", "type": "text"},
+                {
+                    "tool_input": {"a": "c"},
+                    "type": "tool_use",
+                    "id": "2",
+                    "text": None,
+                    "name": "blah",
+                },
+            ]
+        ),
+        ToolMessage("buz output", tool_call_id="1"),  # type: ignore[misc]
+        ToolMessage("blah output", tool_call_id="2"),  # type: ignore[misc]
+        HumanMessage("next thing"),  # type: ignore[misc]
+    ]
+    expected = [
+        SystemMessage("foo"),  # type: ignore[misc]
+        HumanMessage("bar"),  # type: ignore[misc]
+        AIMessage(  # type: ignore[misc]
+            [
+                {"text": "baz", "type": "text"},
+                {
+                    "tool_input": {"a": "b"},
+                    "type": "tool_use",
+                    "id": "1",
+                    "text": None,
+                    "name": "buz",
+                },
+                {"text": "baz", "type": "text"},
+                {
+                    "tool_input": {"a": "c"},
+                    "type": "tool_use",
+                    "id": "2",
+                    "text": None,
+                    "name": "blah",
+                },
+            ]
+        ),
+        HumanMessage(  # type: ignore[misc]
+            [
+                {"type": "tool_result", "content": "buz output", "tool_use_id": "1"},
+                {"type": "tool_result", "content": "blah output", "tool_use_id": "2"},
+                {"type": "text", "text": "next thing"},
+            ]
+        ),
+    ]
+    actual = _merge_messages(messages)
+    assert expected == actual
+
+
+def test__merge_messages_mutation() -> None:
+    original_messages = [
+        HumanMessage([{"type": "text", "text": "bar"}]),  # type: ignore[misc]
+        HumanMessage("next thing"),  # type: ignore[misc]
+    ]
+    messages = [
+        HumanMessage([{"type": "text", "text": "bar"}]),  # type: ignore[misc]
+        HumanMessage("next thing"),  # type: ignore[misc]
+    ]
+    expected = [
+        HumanMessage(  # type: ignore[misc]
+            [{"type": "text", "text": "bar"}, {"type": "text", "text": "next thing"}]
+        ),
+    ]
+    actual = _merge_messages(messages)
+    assert expected == actual
+    assert messages == original_messages
+
+
+def test__format_anthropic_messages_with_tool_calls() -> None:
+    system = SystemMessage("fuzz")  # type: ignore[misc]
+    human = HumanMessage("foo")  # type: ignore[misc]
+    ai = AIMessage(  # type: ignore[misc]
+        "",
+        tool_calls=[{"name": "bar", "id": "1", "args": {"baz": "buzz"}}],
+    )
+    tool = ToolMessage(  # type: ignore[misc]
+        "blurb",
+        tool_call_id="1",
+    )
+    messages = [system, human, ai, tool]
+    expected = (
+        "fuzz",
+        [
+            {"role": "user", "content": "foo"},
+            {
+                "role": "assistant",
+                "content": [
+                    {
+                        "type": "tool_use",
+                        "name": "bar",
+                        "id": "1",
+                        "input": {"baz": "buzz"},
+                    }
+                ],
+            },
+            {
+                "role": "user",
+                "content": [
+                    {"type": "tool_result", "content": "blurb", "tool_use_id": "1"}
+                ],
+            },
+        ],
+    )
+    actual = _format_anthropic_messages(messages)
+    assert expected == actual
+
+
+def test__format_anthropic_messages_with_str_content_and_tool_calls() -> None:
+    system = SystemMessage("fuzz")  # type: ignore[misc]
+    human = HumanMessage("foo")  # type: ignore[misc]
+    # If content and tool_calls are specified and content is a string, then both are
+    # included with content first.
+    ai = AIMessage(  # type: ignore[misc]
+        "thought",
+        tool_calls=[{"name": "bar", "id": "1", "args": {"baz": "buzz"}}],
+    )
+    tool = ToolMessage("blurb", tool_call_id="1")  # type: ignore[misc]
+    messages = [system, human, ai, tool]
+    expected = (
+        "fuzz",
+        [
+            {"role": "user", "content": "foo"},
+            {
+                "role": "assistant",
+                "content": [
+                    {"type": "text", "text": "thought"},
+                    {
+                        "type": "tool_use",
+                        "name": "bar",
+                        "id": "1",
+                        "input": {"baz": "buzz"},
+                    },
+                ],
+            },
+            {
+                "role": "user",
+                "content": [
+                    {"type": "tool_result", "content": "blurb", "tool_use_id": "1"}
+                ],
+            },
+        ],
+    )
+    actual = _format_anthropic_messages(messages)
+    assert expected == actual
+
+
+def test__format_anthropic_messages_with_list_content_and_tool_calls() -> None:
+    system = SystemMessage("fuzz")  # type: ignore[misc]
+    human = HumanMessage("foo")  # type: ignore[misc]
+    # If content and tool_calls are specified and content is a list, then content is
+    # preferred.
+    ai = AIMessage(  # type: ignore[misc]
+        [{"type": "text", "text": "thought"}],
+        tool_calls=[{"name": "bar", "id": "1", "args": {"baz": "buzz"}}],
+    )
+    tool = ToolMessage(  # type: ignore[misc]
+        "blurb",
+        tool_call_id="1",
+    )
+    messages = [system, human, ai, tool]
+    expected = (
+        "fuzz",
+        [
+            {"role": "user", "content": "foo"},
+            {
+                "role": "assistant",
+                "content": [{"type": "text", "text": "thought"}],
+            },
+            {
+                "role": "user",
+                "content": [
+                    {"type": "tool_result", "content": "blurb", "tool_use_id": "1"}
+                ],
+            },
+        ],
+    )
+    actual = _format_anthropic_messages(messages)
+    assert expected == actual
+
+
+def test__format_anthropic_messages_with_tool_use_blocks_and_tool_calls() -> None:
+    """Show that tool_calls are preferred to tool_use blocks when both have same id."""
+    system = SystemMessage("fuzz")  # type: ignore[misc]
+    human = HumanMessage("foo")  # type: ignore[misc]
+    # NOTE: tool_use block in contents and tool_calls have different arguments.
+    ai = AIMessage(  # type: ignore[misc]
+        [
+            {"type": "text", "text": "thought"},
+            {
+                "type": "tool_use",
+                "name": "bar",
+                "id": "1",
+                "input": {"baz": "NOT_BUZZ"},
+            },
+        ],
+        tool_calls=[{"name": "bar", "id": "1", "args": {"baz": "BUZZ"}}],
+    )
+    tool = ToolMessage("blurb", tool_call_id="1")  # type: ignore[misc]
+    messages = [system, human, ai, tool]
+    expected = (
+        "fuzz",
+        [
+            {"role": "user", "content": "foo"},
+            {
+                "role": "assistant",
+                "content": [
+                    {"type": "text", "text": "thought"},
+                    {
+                        "type": "tool_use",
+                        "name": "bar",
+                        "id": "1",
+                        "input": {"baz": "BUZZ"},  # tool_calls value preferred.
+                    },
+                ],
+            },
+            {
+                "role": "user",
+                "content": [
+                    {"type": "tool_result", "content": "blurb", "tool_use_id": "1"}
+                ],
+            },
+        ],
+    )
+    actual = _format_anthropic_messages(messages)
+    assert expected == actual
+
+
+@pytest.fixture()
+def pydantic() -> Type[BaseModel]:
+    class dummy_function(BaseModel):
+        """dummy function"""
+
+        arg1: int = Field(..., description="foo")
+        arg2: Literal["bar", "baz"] = Field(..., description="one of 'bar', 'baz'")
+
+    return dummy_function
+
+
+@pytest.fixture()
+def function() -> Callable:
+    def dummy_function(arg1: int, arg2: Literal["bar", "baz"]) -> None:
+        """dummy function
+
+        Args:
+            arg1: foo
+            arg2: one of 'bar', 'baz'
+        """
+        pass
+
+    return dummy_function
+
+
+@pytest.fixture()
+def dummy_tool() -> BaseTool:
+    class Schema(BaseModel):
+        arg1: int = Field(..., description="foo")
+        arg2: Literal["bar", "baz"] = Field(..., description="one of 'bar', 'baz'")
+
+    class DummyFunction(BaseTool):
+        args_schema: Type[BaseModel] = Schema
+        name: str = "dummy_function"
+        description: str = "dummy function"
+
+        def _run(self, *args: Any, **kwargs: Any) -> Any:
+            pass
+
+    return DummyFunction()
+
+
+@pytest.fixture()
+def json_schema() -> Dict:
+    return {
+        "title": "dummy_function",
+        "description": "dummy function",
+        "type": "object",
+        "properties": {
+            "arg1": {"description": "foo", "type": "integer"},
+            "arg2": {
+                "description": "one of 'bar', 'baz'",
+                "enum": ["bar", "baz"],
+                "type": "string",
+            },
+        },
+        "required": ["arg1", "arg2"],
+    }
+
+
+@pytest.fixture()
+def openai_function() -> Dict:
+    return {
+        "name": "dummy_function",
+        "description": "dummy function",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "arg1": {"description": "foo", "type": "integer"},
+                "arg2": {
+                    "description": "one of 'bar', 'baz'",
+                    "enum": ["bar", "baz"],
+                    "type": "string",
+                },
+            },
+            "required": ["arg1", "arg2"],
+        },
+    }
+
+
+def test_convert_to_anthropic_tool(
+    pydantic: Type[BaseModel],
+    function: Callable,
+    dummy_tool: BaseTool,
+    json_schema: Dict,
+    openai_function: Dict,
+) -> None:
+    expected = {
+        "name": "dummy_function",
+        "description": "dummy function",
+        "input_schema": {
+            "type": "object",
+            "properties": {
+                "arg1": {"description": "foo", "type": "integer"},
+                "arg2": {
+                    "description": "one of 'bar', 'baz'",
+                    "enum": ["bar", "baz"],
+                    "type": "string",
+                },
+            },
+            "required": ["arg1", "arg2"],
+        },
+    }
+
+    for fn in (pydantic, function, dummy_tool, json_schema, expected, openai_function):
+        actual = convert_to_anthropic_tool(fn)  # type: ignore[arg-type]
+        assert actual == expected
+
+
+class GetWeather(BaseModel):
+    """Get the current weather in a given location"""
+
+    location: str = Field(..., description="The city and state, e.g. San Francisco, CA")
+
+
+def test_anthropic_bind_tools_tool_choice() -> None:
+    chat_model = ChatBedrock(
+        model_id="anthropic.claude-3-opus-20240229", region_name="us-west-2"
+    )  # type: ignore[call-arg]
+    chat_model_with_tools = chat_model.bind_tools(
+        [GetWeather], tool_choice={"type": "tool", "name": "GetWeather"}
+    )
+    assert cast(RunnableBinding, chat_model_with_tools).kwargs["tool_choice"] == {
+        "type": "tool",
+        "name": "GetWeather",
+    }
+    chat_model_with_tools = chat_model.bind_tools(
+        [GetWeather], tool_choice="GetWeather"
+    )
+    assert cast(RunnableBinding, chat_model_with_tools).kwargs["tool_choice"] == {
+        "type": "tool",
+        "name": "GetWeather",
+    }
+    chat_model_with_tools = chat_model.bind_tools([GetWeather], tool_choice="auto")
+    assert cast(RunnableBinding, chat_model_with_tools).kwargs["tool_choice"] == {
+        "type": "auto"
+    }
+    chat_model_with_tools = chat_model.bind_tools([GetWeather], tool_choice="any")
+    assert cast(RunnableBinding, chat_model_with_tools).kwargs["tool_choice"] == {
+        "type": "any"
+    }
