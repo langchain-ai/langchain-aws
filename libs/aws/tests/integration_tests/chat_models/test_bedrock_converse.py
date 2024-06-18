10068ca007d564ab02c58d4536a3487877c708fd
Bagatur <baskaryan@gmail.com>
2024-06-17 17:02:27 -0700

    fmt

--- libs/aws/tests/integration_tests/chat_models/test_bedrock_converse.py
+++ libs/aws/tests/integration_tests/chat_models/test_bedrock_converse.py
@@ -2,7 +2,6 @@
 
 from typing import Type
 
-import pytest
 from langchain_core.language_models import BaseChatModel
 from langchain_standard_tests.integration_tests import ChatModelIntegrationTests
 
@@ -10,12 +9,18 @@ from langchain_aws import ChatBedrockConverse
 
 
 class TestBedrockStandard(ChatModelIntegrationTests):
-    @pytest.fixture
+    @property
     def chat_model_class(self) -> Type[BaseChatModel]:
         return ChatBedrockConverse
 
-    @pytest.fixture
+    @property
     def chat_model_params(self) -> dict:
+        return {"model_id": "anthropic.claude-3-sonnet-20240229-v1:0"}
+
+    @property
+    def standard_chat_model_params(self) -> dict:
         return {
-            "model_id": "anthropic.claude-3-sonnet-20240229-v1:0",
+            "temperature": 0,
+            "max_tokens": 100,
+            "stop_sequences": [],
         }
