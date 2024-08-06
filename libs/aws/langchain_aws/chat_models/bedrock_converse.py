683baabce777f16fd513ed73c7415f9af3109e31
Piyush Jain <piyushjain@duck.com>
2024-08-05 22:46:33 -0700

    fmt

--- libs/aws/langchain_aws/chat_models/bedrock_converse.py
+++ libs/aws/langchain_aws/chat_models/bedrock_converse.py
@@ -747,9 +747,7 @@ def _anthropic_to_bedrock(
         elif block["type"] == "image":
             # Assume block is already in bedrock format.
             if "image" in block:
-                bedrock_content.append({
-                    "image": block["image"]
-                })
+                bedrock_content.append({"image": block["image"]})
             else:
                 bedrock_content.append(
                     {
@@ -768,9 +766,7 @@ def _anthropic_to_bedrock(
             )
         elif block["type"] == "document":
             # Assume block in bedrock document format
-            bedrock_content.append({
-                "document": block["document"]
-            })
+            bedrock_content.append({"document": block["document"]})
         elif block["type"] == "tool_use":
             bedrock_content.append(
                 {
@@ -822,12 +818,7 @@ def _bedrock_to_anthropic(content: List[Dict[str, Any]]) -> List[Dict[str, Any]]
                 }
             )
         elif "document" in block:
-            anthropic_content.append(
-                {
-                    "type": "document",
-                    "document": block
-                }
-            )
+            anthropic_content.append({"type": "document", "document": block})
         elif "tool_result" in block:
             anthropic_content.append(
                 {
