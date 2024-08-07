1107e16183dfffb26dcc66d05202c6c776c8d7a0
Bagatur <22008038+baskaryan@users.noreply.github.com>
2024-08-07 10:16:32 -0700

    aws[patch]: fix streaming and tool choice for non-anthropic models (#142)
    Fixes #140

--- libs/aws/langchain_aws/chat_models/bedrock_converse.py
+++ libs/aws/langchain_aws/chat_models/bedrock_converse.py
@@ -334,17 +334,38 @@ class ChatBedrockConverse(BaseChatModel):
     additionalModelResponseFieldPaths.
     """
 
+    supports_tool_choice_values: Optional[
+        Sequence[Literal["auto", "any", "tool"]]
+    ] = None
+    """Which types of tool_choice values the model supports.
+    
+    Inferred if not specified. Inferred as ('auto', 'any', 'tool') if a 'claude-3' 
+    model is used, ('auto', 'any') if a 'mistral-large' model is used, empty otherwise.
+    """
+
     class Config:
         """Configuration for this pydantic object."""
 
         extra = Extra.forbid
         allow_population_by_field_name = True
 
+    @root_validator(pre=True)
+    def set_disable_streaming(cls, values: Dict) -> Dict:
+        values["provider"] = (
+            values.get("provider")
+            or (values.get("model_id", values["model"])).split(".")[0]
+        )
+
+        # As of 08/05/24 only Anthropic models support streamed tool calling
+        if "disable_streaming" not in values:
+            values["disable_streaming"] = (
+                False if "anthropic" in values["provider"] else "tool_calling"
+            )
+        return values
+
     @root_validator(pre=False, skip_on_failure=True)
     def validate_environment(cls, values: Dict) -> Dict:
         """Validate that AWS credentials to and python package exists in environment."""
-        values["provider"] = values["provider"] or values["model_id"].split(".")[0]
-
         if values["client"] is not None:
             return values
 
@@ -387,6 +408,16 @@ class ChatBedrockConverse(BaseChatModel):
                 "Please check that credentials in the specified "
                 f"profile name are valid. Bedrock error: {e}"
             ) from e
+
+        # As of 08/05/24 only claude-3 and mistral-large models support tool choice:
+        # https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_ToolChoice.html
+        if values["supports_tool_choice_values"] is None:
+            if "claude-3" in values["model_id"]:
+                values["supports_tool_choice_values"] = ("auto", "any", "tool")
+            elif "mistral-large" in values["model_id"]:
+                values["supports_tool_choice_values"] = ("auto", "any")
+            else:
+                values["supports_tool_choice_values"] = ()
 
         return values
 
@@ -436,6 +467,25 @@ class ChatBedrockConverse(BaseChatModel):
         **kwargs: Any,
     ) -> Runnable[LanguageModelInput, BaseMessage]:
         if tool_choice:
+            tool_choice = _format_tool_choice(tool_choice)
+            tool_choice_type = list(tool_choice.keys())[0]
+            if tool_choice_type not in list(self.supports_tool_choice_values or []):
+                if self.supports_tool_choice_values:
+                    supported = (
+                        f"Model {self.model_id} does not currently support tool_choice "
+                        f"of type {tool_choice_type}. The following tool_choice types "
+                        f"are supported: {self.supports_tool_choice_values}."
+                    )
+                else:
+                    supported = (
+                        f"Model {self.model_id} does not currently support tool_choice."
+                    )
+
+                raise ValueError(
+                    f"{supported} Please see "
+                    f"https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_ToolChoice.html "  # noqa: E501
+                    f"for the latest documentation on models that support tool choice."
+                )
             kwargs["tool_choice"] = _format_tool_choice(tool_choice)
         return self.bind(tools=_format_tools(tools), **kwargs)
 
@@ -446,8 +496,14 @@ class ChatBedrockConverse(BaseChatModel):
         include_raw: bool = False,
         **kwargs: Any,
     ) -> Runnable[LanguageModelInput, Union[Dict, BaseModel]]:
-        tool_name = convert_to_openai_function(schema)["name"]
-        llm = self.bind_tools([schema], tool_choice=tool_name)
+        supports_tool_choice_values = self.supports_tool_choice_values or ()
+        if "tool" in supports_tool_choice_values:
+            tool_choice = convert_to_openai_function(schema)["name"]
+        elif "any" in supports_tool_choice_values:
+            tool_choice = "any"
+        else:
+            tool_choice = None
+        llm = self.bind_tools([schema], tool_choice=tool_choice)
         if isinstance(schema, type) and is_basemodel_subclass(schema):
             output_parser = ToolsOutputParser(
                 first_tool_only=True, pydantic_schemas=[schema]
