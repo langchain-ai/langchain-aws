a5e17a61182129d122fa1863457d9182c2712c2e
Chester Curme <chester.curme@gmail.com>
2024-09-05 09:16:36 -0400

    format

--- libs/aws/langchain_aws/chat_models/bedrock_converse.py
+++ libs/aws/langchain_aws/chat_models/bedrock_converse.py
@@ -394,9 +394,7 @@ class ChatBedrockConverse(BaseChatModel):
             ) from e
 
         self.region_name = (
-            self.region_name
-            or os.getenv("AWS_DEFAULT_REGION")
-            or session.region_name
+            self.region_name or os.getenv("AWS_DEFAULT_REGION") or session.region_name
         )
 
         client_params = {}
