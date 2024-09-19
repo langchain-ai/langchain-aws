e9a8f6b5b634aac1a2650e79a832bb3a7d4df37f
Chester Curme <chester.curme@gmail.com>
2024-09-19 12:25:20 -0400

    rename validator

--- libs/aws/langchain_aws/llms/bedrock.py
+++ libs/aws/langchain_aws/llms/bedrock.py
@@ -959,7 +959,7 @@ class BedrockLLM(LLM, BedrockBase):
     """
 
     @model_validator(mode="after")
-    def validate_environment(self) -> Self:
+    def validate_environment_llm(self) -> Self:
         model_id = self.model_id
         if model_id.startswith("anthropic.claude-3"):
             raise ValueError(
