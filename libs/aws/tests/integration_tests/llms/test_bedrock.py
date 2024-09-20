6ad78b78133667f103b8ca3145c083e358fad96b
Bagatur <22008038+baskaryan@users.noreply.github.com>
2024-09-20 06:00:24 -0700

    aws[patch]: support ChatBedrock(model=...) (#211)
    per https://github.com/langchain-ai/langchain/issues/20085
    
    ---------
    
    Co-authored-by: Chester Curme <chester.curme@gmail.com>

--- libs/aws/tests/integration_tests/llms/test_bedrock.py
+++ libs/aws/tests/integration_tests/llms/test_bedrock.py
@@ -2,7 +2,7 @@ from langchain_aws import BedrockLLM
 
 
 def test_bedrock_llm() -> None:
-    llm = BedrockLLM(model_id="anthropic.claude-v2:1")
+    llm = BedrockLLM(model_id="anthropic.claude-v2:1")  # type: ignore[call-arg]
     response = llm.invoke("Hello")
     assert isinstance(response, str)
     assert len(response) > 0
