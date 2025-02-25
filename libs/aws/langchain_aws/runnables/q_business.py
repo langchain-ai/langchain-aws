c9f0ff80752ba94298206a14ce78b90e61e580e6
Michael Chin <chnmch@amazon.com>
2025-02-25 12:30:08 -0800

    Import fmt

--- libs/aws/langchain_aws/runnables/q_business.py
+++ libs/aws/langchain_aws/runnables/q_business.py
@@ -2,11 +2,11 @@ import logging
 from typing import Any, Dict, Optional, Union
 
 from langchain_core._api.beta_decorator import beta
+from langchain_core.prompt_values import BaseMessage
 from langchain_core.runnables import Runnable
 from langchain_core.runnables.config import RunnableConfig
 from pydantic import ConfigDict
 from typing_extensions import Self
-from langchain_core.prompt_values import BaseMessage
 
 
 @beta(message="This API is in beta and can change in future.")
