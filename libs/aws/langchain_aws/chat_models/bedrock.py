1cec64323e4c88f7b3d1ec0f4908c56c84d4a6e0
Michael Chin <mchin188@yahoo.com>
2025-02-26 12:08:18 -0800

    Add support for DeepSeek-R1 models (#353)
    Addresses #352.
    
    Updated ChatBedrock's ChatPromptAdapter to apply optimal message
    templating to DeepSeek-R1 prompts.
    
    ---------
    
    Co-authored-by: Piyush Jain <piyushjain@duck.com>

--- libs/aws/langchain_aws/chat_models/bedrock.py
+++ libs/aws/langchain_aws/chat_models/bedrock.py
@@ -185,6 +185,41 @@ def convert_messages_to_prompt_mistral(messages: List[BaseMessage]) -> str:
     )
 
 
+def _convert_one_message_to_text_deepseek(message: BaseMessage) -> str:
+    if isinstance(message, ChatMessage):
+        message_text = (
+            f"<|{message.role}|>{message.content}"
+        )
+    elif isinstance(message, HumanMessage):
+        message_text = (
+            f"<|User|>{message.content}"
+        )
+    elif isinstance(message, AIMessage):
+        message_text = (
+            f"<|Assistant|>{message.content}"
+        )
+    elif isinstance(message, SystemMessage):
+        message_text = (
+            f"<|System|>{message.content}"
+        )
+    else:
+        raise ValueError(f"Got unknown type {message}")
+
+    return message_text
+
+
+def convert_messages_to_prompt_deepseek(messages: List[BaseMessage]) -> str:
+    """Convert a list of messages to a prompt for DeepSeek-R1."""
+    prompt = "\n<|begin_of_sentence|>"
+
+    for message in messages:
+        prompt += _convert_one_message_to_text_deepseek(message)
+
+    prompt += "<|Assistant|>\n\n"
+
+    return prompt
+
+
 def _format_image(image_url: str) -> Dict:
     """
     Formats an image of format data:image/jpeg;base64,{b64_string}
@@ -350,6 +385,8 @@ class ChatPromptAdapter:
     ) -> str:
         if provider == "anthropic":
             prompt = convert_messages_to_prompt_anthropic(messages=messages)
+        elif provider == "deepseek":
+            prompt = convert_messages_to_prompt_deepseek(messages=messages)
         elif provider == "meta":
             if "llama3" in model:
                 prompt = convert_messages_to_prompt_llama3(messages=messages)
