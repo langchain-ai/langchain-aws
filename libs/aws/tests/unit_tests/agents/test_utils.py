76639dceb6255297c2287e117878b1061d64ce68
Michael Chin <mchin188@yahoo.com>
2025-03-05 18:15:24 -0800

    fmt

--- libs/aws/tests/unit_tests/agents/test_utils.py
+++ libs/aws/tests/unit_tests/agents/test_utils.py
@@ -3,14 +3,14 @@ from botocore.config import Config
 from langchain_aws.agents.utils import SDK_USER_AGENT, get_boto_session
 
 
-def test_get_boto3_session():
+def test_get_boto3_session() -> None:
     client_params, session = get_boto_session()
     assert "config" in client_params
     config = client_params["config"]
     assert SDK_USER_AGENT in config.user_agent_extra
 
 
-def test_get_boto_session_with_config():
+def test_get_boto_session_with_config() -> None:
     # Set default client parameters
     fake_config = Config(
         connect_timeout=240,
@@ -26,7 +26,7 @@ def test_get_boto_session_with_config():
     assert config.retries["max_attempts"] == fake_config.retries["max_attempts"]
 
 
-def test_get_boto_session_with_user_agent():
+def test_get_boto_session_with_user_agent() -> None:
     # Set default client parameters
     fake_config = Config(
         connect_timeout=240,
