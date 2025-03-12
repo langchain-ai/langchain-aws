4d17c756b3d094a53b39939b2f4377e6139a0a7a
Nicolas Bernier <nbernier99@gmail.com>
2025-03-12 11:57:23 -0400

    updated comments

--- libs/aws/langchain_aws/chat_models/sagemaker_endpoint.py
+++ libs/aws/langchain_aws/chat_models/sagemaker_endpoint.py
@@ -65,7 +65,7 @@ class ChatSagemakerEndpoint(BaseChatModel):
 
         client: boto3 client for Sagemaker Endpoint
 
-        content_handler: Implementation for model specific LLMContentHandler 
+        content_handler: Implementation for model specific ChatContentHandler 
 
 
     Example:
@@ -143,9 +143,9 @@ class ChatSagemakerEndpoint(BaseChatModel):
      Example:
         .. code-block:: python
 
-        from langchain_community.llms.sagemaker_endpoint import LLMContentHandler
+        from langchain_community.llms.sagemaker_endpoint import ChatContentHandler
 
-        class ContentHandler(LLMContentHandler):
+        class ContentHandler(ChatContentHandler):
                 content_type = "application/json"
                 accepts = "application/json"
 
