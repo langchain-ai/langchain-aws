33854bd296fce057f4476f95d0026c657779e53f
Michael Chin <chnmch@amazon.com>
2025-03-13 13:47:02 -0700

    Switch to pydantic version bump

--- libs/aws/langchain_aws/document_compressors/rerank.py
+++ libs/aws/langchain_aws/document_compressors/rerank.py
@@ -29,7 +29,6 @@ class BedrockRerank(BaseDocumentCompressor):
     model_config = ConfigDict(
         extra="forbid",
         arbitrary_types_allowed=True,
-        protected_namespaces=(),
     )
 
     @model_validator(mode="before")
