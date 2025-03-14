ef2a01d5e1427661a0f0a5d9d0b7320af13b524e
ccurme <chester.curme@gmail.com>
2025-03-14 12:15:14 -0400

    aws[patch]: update langchain-tests (#396)
    Update `langchain-tests`, which includes an update testing that
    structured output formats are traced when using
    `with_structured_output`.
    
    ---------
    
    Co-authored-by: Michael Chin <mchin188@yahoo.com>

--- libs/aws/langchain_aws/chat_models/bedrock.py
+++ libs/aws/langchain_aws/chat_models/bedrock.py
@@ -37,6 +37,7 @@ from langchain_core.messages.tool import ToolCall, ToolMessage
 from langchain_core.outputs import ChatGeneration, ChatGenerationChunk, ChatResult
 from langchain_core.runnables import Runnable, RunnableMap, RunnablePassthrough
 from langchain_core.tools import BaseTool
+from langchain_core.utils.function_calling import convert_to_openai_tool
 from langchain_core.utils.pydantic import TypeBaseModel, is_basemodel_subclass
 from pydantic import BaseModel, ConfigDict, Field, model_validator
 
@@ -942,7 +943,14 @@ class ChatBedrock(BaseChatModel, BedrockBase):
             )
 
         tool_name = convert_to_anthropic_tool(schema)["name"]
-        llm = self.bind_tools([schema], tool_choice=tool_name)
+        llm = self.bind_tools(
+            [schema],
+            tool_choice=tool_name,
+            ls_structured_output_format={
+                "kwargs": {"method": "function_calling"},
+                "schema": convert_to_openai_tool(schema),
+            },
+        )
         if isinstance(schema, type) and is_basemodel_subclass(schema):
             output_parser = ToolsOutputParser(
                 first_tool_only=True, pydantic_schemas=[schema]
