858ad4ed35768ac16842df95f344894d7b488567
Stuart Axon <stuaxo2@yahoo.com>
2025-03-26 21:00:17 +0000

    Update libs/aws/langchain_aws/chat_models/bedrock_converse.py
    Don't use Exception handler when sanity checking data from AWS endpoint.
    
    Co-authored-by: Piyush Jain <piyushjain@duck.com>

--- libs/aws/langchain_aws/chat_models/bedrock_converse.py
+++ libs/aws/langchain_aws/chat_models/bedrock_converse.py
@@ -943,16 +943,14 @@ def _extract_response_metadata(response: Dict[str, Any]) -> Dict[str, Any]:
 
 
 def _parse_response(response: Dict[str, Any]) -> AIMessage:
-    try:
-        response_content = response.pop("output")["message"]["content"]
-    except KeyError:
+    if "output" not in response:
         raise ValueError(
             "No 'output' key found in the response from the Bedrock Converse API.  This usually "
             "happens due to misconfiguration of endpoint or region, ensure that you are using valid "
             "values for endpoint_url (on AWS this starts with bedrock-runtime), see: "
             "https://docs.aws.amazon.com/general/latest/gr/bedrock.html"
         )
-    lc_content = _bedrock_to_lc(response_content)
+    lc_content = _bedrock_to_lc(response.pop("output")["message"]["content"])
     tool_calls = _extract_tool_calls(lc_content)
     usage = UsageMetadata(_camel_to_snake_keys(response.pop("usage")))  # type: ignore[misc]
     return AIMessage(
