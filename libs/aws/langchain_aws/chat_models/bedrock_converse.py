b7575f371c8f44766ad4473f2785caaef583323d
ccurme <chester.curme@gmail.com>
2025-03-27 16:35:19 -0400

    aws[patch]: support snake_case arguments in tools in ChatBedrockConverse (#441)
    Following https://github.com/langchain-ai/langchain-aws/pull/437

--- libs/aws/langchain_aws/chat_models/bedrock_converse.py
+++ libs/aws/langchain_aws/chat_models/bedrock_converse.py
@@ -592,7 +592,7 @@ class ChatBedrockConverse(BaseChatModel):
         logger.debug(f"input message to bedrock: {bedrock_messages}")
         logger.debug(f"System message to bedrock: {system}")
         params = self._converse_params(
-            stop=stop, **_snake_to_camel_keys(kwargs, excluded_keys={"inputSchema", "function"})
+            stop=stop, **_snake_to_camel_keys(kwargs, excluded_keys={"inputSchema", "properties"})
         )
         logger.debug(f"Input params: {params}")
         logger.info("Using Bedrock Converse API to generate response")
@@ -613,7 +613,7 @@ class ChatBedrockConverse(BaseChatModel):
     ) -> Iterator[ChatGenerationChunk]:
         bedrock_messages, system = _messages_to_bedrock(messages)
         params = self._converse_params(
-            stop=stop, **_snake_to_camel_keys(kwargs, excluded_keys={"inputSchema", "function"})
+            stop=stop, **_snake_to_camel_keys(kwargs, excluded_keys={"inputSchema", "properties"})
         )
         response = self.client.converse_stream(
             messages=bedrock_messages, system=system, **params
