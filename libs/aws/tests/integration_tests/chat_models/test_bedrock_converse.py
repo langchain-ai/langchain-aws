86f94e8a5115dd66dac227408306b1718c592709
Chester Curme <chester.curme@gmail.com>
2025-03-27 16:12:55 -0400

    lint

--- libs/aws/tests/integration_tests/chat_models/test_bedrock_converse.py
+++ libs/aws/tests/integration_tests/chat_models/test_bedrock_converse.py
@@ -5,7 +5,7 @@ from typing import Literal, Optional, Type
 import pytest
 from langchain_core.exceptions import OutputParserException
 from langchain_core.language_models import BaseChatModel
-from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
+from langchain_core.messages import AIMessage, AIMessageChunk, HumanMessage
 from langchain_core.tools import BaseTool
 from langchain_tests.integration_tests import ChatModelIntegrationTests
 from pydantic import BaseModel, Field
@@ -195,10 +195,10 @@ def test_tool_calling_snake_case() -> None:
     assert tool_call["name"] == "classify_query"
     assert tool_call["args"] == {"query_type": "cat"}
 
-    full: Optional[BaseMessage] = None
+    full = None
     for chunk in chat.stream("How big are cats?"):
-        full = chunk if full is None else full + chunk
-    assert isinstance(full, AIMessage)
+        full = chunk if full is None else full + chunk  # type: ignore[operator]
+    assert isinstance(full, AIMessageChunk)
     assert len(full.tool_calls) == 1
     tool_call = full.tool_calls[0]
     assert tool_call["name"] == "classify_query"
