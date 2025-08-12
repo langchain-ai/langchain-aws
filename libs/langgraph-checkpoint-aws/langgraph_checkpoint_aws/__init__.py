509d2c1a0ee92dc46e0c4b7aeb1127678d75dcbb
Michael Chin <mchin188@yahoo.com>
2025-08-11 18:50:32 -0700

    langgraph-checkpoint-aws: release 0.1.1 (#578)
    Fixes: #546 
    
    This is a patch release to unblock usage of `langgraph-checkpoint-aws`
    with LangGraph >=0.5.
    
    A more comprehensive update for LangGraph >=0.5 compatibility is pending
    investigation with
    https://github.com/langchain-ai/langchain-aws/pull/550.

--- libs/langgraph-checkpoint-aws/langgraph_checkpoint_aws/__init__.py
+++ libs/langgraph-checkpoint-aws/langgraph_checkpoint_aws/__init__.py
@@ -2,5 +2,5 @@
 LangGraph Checkpoint AWS - A LangChain checkpointer implementation using Bedrock Session Management Service.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 SDK_USER_AGENT = f"LangGraphCheckpointAWS#{__version__}"
